9153e47267a8c68722d0448b3cc2e5c69c19b647
Wolverine971 <dwayne@metrostarsystems.com>
2019-01-14 13:47:11 -0500

    updated the map key

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  MAP_KEY: 'pk.eyJ1IjoiZGp3YXluZTMiLCJhIjoiY2pxc2ppeXQyMGttOTQzcXZvdjBuZHZweSJ9.kWjE2XSfLBrUAHDaeliJ-Q'
 })
